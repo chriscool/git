d740ceb08e3e9451278a7f93120ba2f71c536d47
Christian Couder <chriscool@tuxfamily.org>
2016-05-25 15:41:01 +0200

    t0400: add 'put' command to odb-helper script
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- t/t0400-external-odb.sh
+++ t/t0400-external-odb.sh
@@ -7,6 +7,10 @@ test_description='basic tests for external object databases'
 ALT_SOURCE="$PWD/alt-repo/.git"
 export ALT_SOURCE
 write_script odb-helper <<\EOF
+die() {
+	printf >&2 "%s\n" "$@"
+	exit 1
+}
 GIT_DIR=$ALT_SOURCE; export GIT_DIR
 case "$1" in
 have)
@@ -15,6 +19,16 @@ have)
 	;;
 get)
 	cat "$GIT_DIR"/objects/$(echo $2 | sed 's#..#&/#')
+	;;
+put)
+	sha1="$2"
+	size="$3"
+	kind="$4"
+	writen=$(git hash-object -w -t "$kind" --stdin)
+	test "$writen" = "$sha1" || die "bad sha1 passed '$sha1' vs writen '$writen'"
+	;;
+*)
+	die "unknown command '$1'"
 	;;
 esac
 EOF
@@ -41,6 +55,15 @@ test_expect_success 'helper can retrieve alt objects' '
 	EOF
 	git log --format=%s $alt_head >actual &&
 	test_cmp expect actual
+'
+
+test_expect_success 'helper can add objects to alt repo' '
+	hash=$(echo "Hello odb!" | git hash-object -w -t blob --stdin) &&
+	test -f .git/objects/$(echo $hash | sed "s#..#&/#") &&
+	size=$(git cat-file -s "$hash") &&
+	git cat-file blob "$hash" | ./odb-helper put "$hash" "$size" blob &&
+	alt_size=$(cd alt-repo && git cat-file -s "$hash") &&
+	test "$size" -eq "$alt_size"
 '
 
 test_done
