196b1b283e8569d2f4e4deb4ce4586daae3a8504
Christian Couder <chriscool@tuxfamily.org>
2016-05-27 16:57:57 +0200

    external odb: add 'put_raw_obj' support
    Add support for a 'put_raw_obj' capability/instruction to send new
    objects to an external odb. Objects will be sent as they are (in
    their 'raw' format). They will not be converted to Git objects.
    
    For now any new Git object (blob, tree, commit, ...) would be sent
    if 'put_raw_obj' is supported by an odb helper. This is not a great
    default, but let's leave it to following commits to tweak that.
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- external-odb.c
+++ external-odb.c
@@ -118,3 +118,18 @@ int external_odb_get_object(const unsigned char *sha1)
 
 	return -1;
 }
+
+int external_odb_put_object(const void *buf, size_t len,
+			    const char *type, unsigned char *sha1)
+{
+	struct odb_helper *o;
+
+	external_odb_init();
+
+	for (o = helpers; o; o = o->next) {
+		int r = odb_helper_put_object(o, buf, len, type, sha1);
+		if (r <= 0)
+			return r;
+	}
+	return 1;
+}
