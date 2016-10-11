97219c9f9a32e214c4b3df24e60fc4016510f97f
Christian Couder <chriscool@tuxfamily.org>
2016-10-11 18:22:37 +0200

    lib-httpd: add list.sh
    This cgi script can list Git objects that have been uploaded as
    files to an apache web server. This script can also retrieve
    the content of each of these files.
    
    This will help make apache work as an external object database.
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

---
+++ t/lib-httpd/list.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+FILES_DIR="www/files"
+
+OLDIFS="$IFS"
+IFS='&'
+set -- $QUERY_STRING
+IFS="$OLDIFS"
+
+while test $# -gt 0
+do
+	key=${1%%=*}
+	val=${1#*=}
+
+	case "$key" in
+	"sha1") sha1="$val" ;;
+	*) echo >&2 "unknown key '$key'" ;;
+	esac
+
+	shift
+done
+
+if test -d "$FILES_DIR"
+then
+	if test -z "$sha1"
+	then
+		echo 'Status: 200 OK'
+		echo
+		ls "$FILES_DIR" | tr '-' ' '
+	else
+		if test -f "$FILES_DIR/$sha1"-*
+		then
+			echo 'Status: 200 OK'
+			echo
+			cat "$FILES_DIR/$sha1"-*
+		else
+			echo 'Status: 404 Not Found'
+			echo
+		fi
+	fi
+fi
