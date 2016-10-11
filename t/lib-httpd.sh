7b16f9cfadf5feb2d90bc65fad06ce341b1232e5
Christian Couder <chriscool@tuxfamily.org>
2016-10-11 18:22:37 +0200

    lib-httpd: add list.sh
    This cgi script can list Git objects that have been uploaded as
    files to an apache web server. This script can also retrieve
    the content of each of these files.
    
    This will help make apache work as an external object database.
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- t/lib-httpd.sh
+++ t/lib-httpd.sh
@@ -134,6 +134,7 @@ prepare_httpd() {
 	install_script error.sh
 	install_script apply-one-time-sed.sh
 	install_script upload.sh
+	install_script list.sh
 
 	ln -s "$LIB_HTTPD_MODULE_PATH" "$HTTPD_ROOT_PATH/modules"
 
