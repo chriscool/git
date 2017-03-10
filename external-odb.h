21c23a2c37af3e4e0bc7353b654c9a098abffaa0
Christian Couder <chriscool@tuxfamily.org>
2017-03-10 11:33:15 +0100

    external-odb: add external_odb_fault_in_object()
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- external-odb.h
+++ external-odb.h
@@ -4,6 +4,7 @@
 const char *external_odb_root(void);
 int external_odb_has_object(const unsigned char *sha1);
 int external_odb_fetch_object(const unsigned char *sha1);
+int external_odb_fault_in_object(const unsigned char *sha1);
 int external_odb_write_object(const void *buf, size_t len,
 			      const char *type, unsigned char *sha1);
 
