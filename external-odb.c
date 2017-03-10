de6504602e8f202c442571d8f91c53de29f891b7
Christian Couder <chriscool@tuxfamily.org>
2017-03-10 11:33:15 +0100

    external-odb: add external_odb_fault_in_object()
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- external-odb.c
+++ external-odb.c
@@ -126,7 +126,8 @@ int external_odb_fetch_object(const unsigned char *sha1)
 		int ret;
 		int fd;
 
-		if (!odb_helper_has_object(o, sha1))
+		if (o->fetch_kind != ODB_FETCH_KIND_PLAIN_OBJECT &&
+		    o->fetch_kind != ODB_FETCH_KIND_GIT_OBJECT)
 			continue;
 
 		fd = create_object_tmpfile(&tmpfile, path);
@@ -147,6 +148,24 @@ int external_odb_fetch_object(const unsigned char *sha1)
 		strbuf_release(&tmpfile);
 		if (!ret)
 			return 0;
+	}
+
+	return -1;
+}
+
+int external_odb_fault_in_object(const unsigned char *sha1)
+{
+	struct odb_helper *o;
+
+	if (!external_odb_has_object(sha1))
+		return -1;
+
+	for (o = helpers; o; o = o->next) {
+		if (o->fetch_kind != ODB_FETCH_KIND_FAULT_IN)
+			continue;
+		if (odb_helper_fault_in_object(o, sha1) < 0)
+			continue;
+		return 0;
 	}
 
 	return -1;
