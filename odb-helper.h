f6f82082628fa6afd53ca45bd6c6f8dc2b9221d2
Christian Couder <chriscool@tuxfamily.org>
2017-03-27 13:38:33 +0200

    odb-helper: add odb_helper_init() to send 'init' instruction
    Let's add an odb_helper_init() function to send an 'init'
    instruction to the helpers. This 'init' instruction is
    especially useful to get the capabilities that are supported
    by the helpers.
    
    So while at it, let's also add a parse_capabilities()
    function to parse them and a supported_capabilities
    variable in struct odb_helper to store them.
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- odb-helper.h
+++ odb-helper.h
@@ -1,9 +1,20 @@
 #ifndef ODB_HELPER_H
 #define ODB_HELPER_H
 
+#include "external-odb.h"
+
+#define ODB_HELPER_CAP_GET_GIT_OBJ    (1u<<0)
+#define ODB_HELPER_CAP_GET_RAW_OBJ    (1u<<1)
+#define ODB_HELPER_CAP_GET_DIRECT     (1u<<2)
+#define ODB_HELPER_CAP_PUT_GIT_OBJ    (1u<<3)
+#define ODB_HELPER_CAP_PUT_RAW_OBJ    (1u<<4)
+#define ODB_HELPER_CAP_PUT_DIRECT     (1u<<5)
+#define ODB_HELPER_CAP_HAVE           (1u<<6)
+
 struct odb_helper {
 	const char *name;
 	const char *cmd;
+	unsigned int supported_capabilities;
 
 	struct odb_helper_object {
 		unsigned char sha1[20];
@@ -18,6 +29,7 @@ struct odb_helper {
 };
 
 struct odb_helper *odb_helper_new(const char *name, int namelen);
+int odb_helper_init(struct odb_helper *o);
 int odb_helper_has_object(struct odb_helper *o, const unsigned char *sha1);
 int odb_helper_get_object(struct odb_helper *o, const unsigned char *sha1,
 			  int fd);
