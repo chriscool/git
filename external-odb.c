d6aa2a51e5a84ff16c6f25cc316493161a2de0f4
Christian Couder <chriscool@tuxfamily.org>
2017-03-27 08:37:25 +0200

    odb-helper: add 'script_mode' to 'struct odb_helper'
    to prepare for having a long running odb helper sub-process
    handling the communication between Git and an external odb.
    
    We introduce "odb.<name>.subprocesscommand" to make it
    possible to define such a sub-process, and we mark such odb
    helpers with the new 'script_mode' field set to 0.
    
    Helpers defined using the existing "odb.<name>.scriptcommand"
    are marked with the 'script_mode' field set to 1.
    
    Implementation of the different capabilities/instructions in
    the new (sub-)process mode is left for following commits.
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- external-odb.c
+++ external-odb.c
@@ -43,6 +43,10 @@ static int external_odb_config(const char *var, const char *value, void *data)
 		o->type = ODB_HELPER_SCRIPT_CMD;
 		return git_config_string(&o->dealer, var, value);
 	}
+	if (!strcmp(subkey, "subprocesscommand")) {
+		o->type = ODB_HELPER_SUBPROCESS_CMD;
+		return git_config_string(&o->dealer, var, value);
+	}
 
 	return 0;
 }
