06d6ba6c1b92aa5db87832aa9654687ed83afd43
Christian Couder <chriscool@tuxfamily.org>
2017-05-19 13:57:03 +0200

    odb-helper: clarify error messages
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- odb-helper.c
+++ odb-helper.c
@@ -401,7 +401,7 @@ done:
 			* Something went wrong with the read-object process.
 			* Force shutdown and restart if needed.
 			*/
-			error("external process '%s' failed", cmd);
+			error("read_object_process: external process '%s' failed", cmd);
 			subprocess_stop(&subprocess_map, &entry->subprocess);
 			free(entry);
 		}
@@ -505,7 +505,7 @@ done:
 			* Something went wrong with the read-object process.
 			* Force shutdown and restart if needed.
 			*/
-			error("external process '%s' failed", cmd);
+			error("write_object_process: external process '%s' failed", cmd);
 			subprocess_stop(&subprocess_map, &entry->subprocess);
 			free(entry);
 		}
