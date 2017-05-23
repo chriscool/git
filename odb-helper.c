3642b5284eeffa59a4500881e92fcf966d4f1029
Christian Couder <chriscool@tuxfamily.org>
2017-05-23 05:59:25 +0200

    odb-helper: fix capability check
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- odb-helper.c
+++ odb-helper.c
@@ -106,7 +106,7 @@ ssize_t read_packetized_plain_object_to_fd(struct odb_helper *o,
 	trace_printf("read_packetized_plain_object_to_fd: start\n");
 
 	/* Get size and object kind first */
-	if (ODB_HELPER_CAP_GET & o->supported_capabilities) {
+	if (ODB_HELPER_CAP_HAVE & o->supported_capabilities) {
 		struct odb_helper_object *obj = odb_helper_lookup(o, sha1);
 		if (!obj) {
 			error("odb helper '%s' lookup error: %s",
