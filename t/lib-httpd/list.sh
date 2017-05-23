c9346e935e35653143a40d1b80a2bf773f2add96
Christian Couder <chriscool@tuxfamily.org>
2017-05-23 06:05:13 +0200

    lib-httpd/list: send 404 when object not found
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- t/lib-httpd/list.sh
+++ t/lib-httpd/list.sh
@@ -20,15 +20,22 @@ do
     shift
 done
 
-echo 'Status: 200 OK'
-echo
-
 if test -d "$FILES_DIR"
 then
-    if test -n "$sha1"
+    if test -z "$sha1"
     then
-	cat "$FILES_DIR/$sha1"-*
-    else
+	echo 'Status: 200 OK'
+	echo
 	ls "$FILES_DIR" | tr '-' ' '
+    else
+	if test -f "$FILES_DIR/$sha1"-*
+	then
+	    echo 'Status: 200 OK'
+	    echo
+	    cat "$FILES_DIR/$sha1"-*
+	else
+	    echo 'Status: 404 Not Found'
+	    echo
+	fi
     fi
 fi
