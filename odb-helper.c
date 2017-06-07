09f8655e70003244659eff1c390b8deb9c48efee
Christian Couder <chriscool@tuxfamily.org>
2017-06-07 14:23:45 +0200

    odb-helper: copy supported_capabilities to object_helper
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- odb-helper.c
+++ odb-helper.c
@@ -330,6 +330,7 @@ static int read_object_process(struct odb_helper *o, const unsigned char *sha1, 
 
 	entry = launch_read_object_process(cmd);
 	process = &entry->subprocess.process;
+	o->supported_capabilities = entry->supported_capabilities;
 
 	if (!(ODB_HELPER_CAP_GET & entry->supported_capabilities))
 		return -1;
@@ -404,6 +405,7 @@ static int write_object_process(struct odb_helper *o,
 
 	entry = launch_read_object_process(cmd);
 	process = &entry->subprocess.process;
+	o->supported_capabilities = entry->supported_capabilities;
 
 	if (!(ODB_HELPER_CAP_PUT & entry->supported_capabilities))
 		return -1;
@@ -615,6 +617,7 @@ static int have_object_process(struct odb_helper *o)
 
 	entry = launch_read_object_process(cmd);
 	process = &entry->subprocess.process;
+	o->supported_capabilities = entry->supported_capabilities;
 
 	if (!(ODB_HELPER_CAP_HAVE & entry->supported_capabilities))
 		return -1;
