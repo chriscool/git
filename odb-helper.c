66b51dffd760b00140e5e1c6606a897a9bbc08b9
Christian Couder <chriscool@tuxfamily.org>
2017-06-14 18:31:38 +0200

    odb-helper: add write_object_process()
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- odb-helper.c
+++ odb-helper.c
@@ -450,6 +450,61 @@ static int read_object_process(struct odb_helper *o, const unsigned char *sha1, 
 					  cur_cap);
 }
 
+static int send_write_packets(struct read_object_process *entry,
+			      const unsigned char *sha1,
+			      const void *buf,
+			      size_t len,
+			      struct strbuf *status)
+{
+	struct child_process *process = &entry->subprocess.process;
+	int err = packet_write_fmt_gently(process->in, "command=put_raw_obj\n");
+	if (err)
+		return err;
+
+	err = packet_write_fmt_gently(process->in, "sha1=%s\n", sha1_to_hex(sha1));
+	if (err)
+		return err;
+
+	err = packet_write_fmt_gently(process->in, "size=%"PRIuMAX"\n", len);
+	if (err)
+		return err;
+
+	err = packet_write_fmt_gently(process->in, "kind=blob\n");
+	if (err)
+		return err;
+
+	err = packet_flush_gently(process->in);
+	if (err)
+		return err;
+
+	err = write_packetized_from_buf(buf, len, process->in);
+	if (err)
+		return err;
+
+	subprocess_read_status(process->out, status);
+
+	return strcmp(status->buf, "success");
+}
+
+static int write_object_process(struct odb_helper *o,
+				const void *buf, size_t len,
+				const char *type, unsigned char *sha1)
+{
+	int err;
+	struct read_object_process *entry;
+	struct strbuf status = STRBUF_INIT;
+
+	entry = launch_read_object_process(o, ODB_HELPER_CAP_PUT_RAW_OBJ);
+	if (!entry)
+		return -1;
+
+	err = send_write_packets(entry, sha1, buf, len, &status);
+
+	return check_object_process_error(err, status.buf,
+					  entry, o->cmd,
+					  ODB_HELPER_CAP_PUT_RAW_OBJ);
+}
+
 struct odb_helper *odb_helper_new(const char *name, int namelen)
 {
 	struct odb_helper *o;
@@ -915,9 +970,9 @@ int odb_helper_for_each_object(struct odb_helper *o,
 	return 0;
 }
 
-int odb_helper_write_object(struct odb_helper *o,
-			    const void *buf, size_t len,
-			    const char *type, unsigned char *sha1)
+int odb_helper_write_plain_object(struct odb_helper *o,
+				  const void *buf, size_t len,
+				  const char *type, unsigned char *sha1)
 {
 	struct odb_helper_cmd cmd;
 
@@ -942,4 +997,21 @@ int odb_helper_write_object(struct odb_helper *o,
 	close(cmd.child.out);
 	odb_helper_finish(o, &cmd);
 	return 0;
+}
+
+int odb_helper_write_object(struct odb_helper *o,
+			    const void *buf, size_t len,
+			    const char *type, unsigned char *sha1)
+{
+	int res;
+	uint64_t start = getnanotime();
+
+	if (o->script_mode)
+		res = odb_helper_write_plain_object(o, buf, len, type, sha1);
+	else
+		res = write_object_process(o, buf, len, type, sha1);
+
+	trace_performance_since(start, "odb_helper_write_object");
+
+	return res;
 }
