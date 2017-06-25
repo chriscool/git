b0e78d745119cd0e3ba8f2a25d97db571f7e4a37
Christian Couder <chriscool@tuxfamily.org>
2017-06-25 20:53:06 +0200

    t0410: use 'scriptCommand' and 'subprocessCommand' config keys
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- t/t0410-transfer-e-odb.sh
+++ t/t0410-transfer-e-odb.sh
@@ -97,8 +97,7 @@ HELPER2="\"$PWD\"/odb-helper2"
 test_expect_success 'setup first alternate repo' '
 	git init alt-repo1 &&
 	test_commit zero &&
-	git config odb.magic.command "$HELPER1" &&
-	git config odb.magic.scriptMode true &&
+	git config odb.magic.scriptCommand "$HELPER1" &&
 	git config odb.magic.fetchKind "gitObject"
 '
 
@@ -128,8 +127,7 @@ test_expect_success 'other repo gets the blobs from object store' '
 	 git fetch origin "refs/odbs/magic/*:refs/odbs/magic/*" &&
 	 test_must_fail git cat-file blob "$hash1" &&
 	 test_must_fail git cat-file blob "$hash2" &&
-	 git config odb.magic.command "$HELPER2" &&
-	 git config odb.magic.scriptMode true &&
+	 git config odb.magic.scriptCommand "$HELPER2" &&
 	 git config odb.magic.fetchKind "gitObject"
 	 git cat-file blob "$hash1" &&
 	 git cat-file blob "$hash2"
