24687d9d55903da473dae7baf768dcf3346f4405
Christian Couder <chriscool@tuxfamily.org>
2017-07-03 11:00:19 +0200

    odb-helper: fix starting helpers
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- odb-helper.h
+++ odb-helper.h
@@ -15,7 +15,6 @@ struct odb_helper {
 	const char *name;
 	const char *cmd;
 	unsigned int supported_capabilities;
-	int store_plain_objects;
 
 	struct odb_helper_object {
 		unsigned char sha1[20];
