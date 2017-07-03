853b1423ca0179705266b009648e52fb23151e4e
Christian Couder <chriscool@tuxfamily.org>
2017-07-03 11:00:57 +0200

    external-odb: fix fetch config
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- external-odb.c
+++ external-odb.c
@@ -34,8 +34,6 @@ static int external_odb_config(const char *var, const char *value, void *data)
 
 	if (!strcmp(subkey, "scriptcommand"))
 		return git_config_string(&o->cmd, var, value);
-	if (!strcmp(key, "plainobjects"))
-		o->store_plain_objects = git_config_bool(var, value);
 
 	return 0;
 }
