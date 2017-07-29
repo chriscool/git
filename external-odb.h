d50a06897f9bdf3913dff20c0431a468551ea43d
Christian Couder <chriscool@tuxfamily.org>
2017-07-29 09:30:22 +0200

    Add initial external odb support
    The external-odb.{c,h} files contains the functions that are
    called by the rest of Git from "sha1_file.c".
    
    The odb-helper.{c,h} files contains the functions to
    actually implement communication with the external scripts or
    processes that will manage external git objects.
    
    For now only script mode is supported, and only the 'have' and
    'get_git_obj' instructions are supported.
    
    Helped-by: Jeff King <peff@peff.net>
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

---
+++ external-odb.h
@@ -0,0 +1,8 @@
+#ifndef EXTERNAL_ODB_H
+#define EXTERNAL_ODB_H
+
+extern const char *external_odb_root(void);
+extern int external_odb_has_object(const unsigned char *sha1);
+extern int external_odb_get_object(const unsigned char *sha1);
+
+#endif /* EXTERNAL_ODB_H */
