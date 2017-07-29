93aa1fe423a54a53c5cde53213007e09e87daa22
Christian Couder <chriscool@tuxfamily.org>
2017-07-29 09:26:36 +0200

    sha1_file: prepare for external odbs
    In the following commits we will need some functions that were
    internal to sha1_file.c, so let's first make them non static
    and declare them in "cache.h". While at it, let's rename
    'create_tmpfile()' to 'create_object_tmpfile()' to make its
    name less generic.
    
    Let's also split out 'sha1_file_name_alt()' from
    'sha1_file_name()' and 'open_sha1_file_alt()' from
    'open_sha1_file()', as we will need both of these new
    functions too.
    
    Helped-by: Jeff King <peff@peff.net>
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- sha1_file.c
+++ sha1_file.c
@@ -320,11 +320,16 @@ static void fill_sha1_path(struct strbuf *buf, const unsigned char *sha1)
 	}
 }
 
-void sha1_file_name(struct strbuf *buf, const unsigned char *sha1)
+void sha1_file_name_alt(struct strbuf *buf, const char *objdir, const unsigned char *sha1)
 {
-	strbuf_addstr(buf, get_object_directory());
+	strbuf_addstr(buf, objdir);
 	strbuf_addch(buf, '/');
 	fill_sha1_path(buf, sha1);
+}
+
+void sha1_file_name(struct strbuf *buf, const unsigned char *sha1)
+{
+	return sha1_file_name_alt(buf, get_object_directory(), sha1);
 }
 
 struct strbuf *alt_scratch_buf(struct alternate_object_database *alt)
@@ -905,28 +910,14 @@ static int stat_sha1_file(const unsigned char *sha1, struct stat *st,
 	return -1;
 }
 
-/*
- * Like stat_sha1_file(), but actually open the object and return the
- * descriptor. See the caveats on the "path" parameter above.
- */
-static int open_sha1_file(const unsigned char *sha1, const char **path)
+static int open_sha1_file_alt(const unsigned char *sha1, const char **path)
 {
-	int fd;
 	struct alternate_object_database *alt;
-	int most_interesting_errno;
-	static struct strbuf buf = STRBUF_INIT;
-
-	strbuf_reset(&buf);
-	sha1_file_name(&buf, sha1);
-	*path = buf.buf;
-
-	fd = git_open(*path);
-	if (fd >= 0)
-		return fd;
-	most_interesting_errno = errno;
+	int most_interesting_errno = errno;
 
 	prepare_alt_odb();
 	for (alt = alt_odb_list; alt; alt = alt->next) {
+		int fd;
 		*path = alt_sha1_path(alt, sha1);
 		fd = git_open(*path);
 		if (fd >= 0)
@@ -936,6 +927,26 @@ static int open_sha1_file(const unsigned char *sha1, const char **path)
 	}
 	errno = most_interesting_errno;
 	return -1;
+}
+
+/*
+ * Like stat_sha1_file(), but actually open the object and return the
+ * descriptor. See the caveats on the "path" parameter above.
+ */
+static int open_sha1_file(const unsigned char *sha1, const char **path)
+{
+	int fd;
+	static struct strbuf buf = STRBUF_INIT;
+
+	strbuf_reset(&buf);
+	sha1_file_name(&buf, sha1);
+	*path = buf.buf;
+
+	fd = git_open(*path);
+	if (fd >= 0)
+		return fd;
+
+	return open_sha1_file_alt(sha1, path);
 }
 
 /*
@@ -1534,7 +1545,7 @@ int hash_object_file(const void *buf, unsigned long len, const char *type,
 }
 
 /* Finalize a file on disk, and close it. */
-static void close_sha1_file(int fd)
+void close_sha1_file(int fd)
 {
 	if (fsync_object_files)
 		fsync_or_die(fd, "sha1 file");
@@ -1558,7 +1569,7 @@ static inline int directory_size(const char *filename)
  * We want to avoid cross-directory filename renames, because those
  * can have problems on various filesystems (FAT, NFS, Coda).
  */
-static int create_tmpfile(struct strbuf *tmp, const char *filename)
+int create_object_tmpfile(struct strbuf *tmp, const char *filename)
 {
 	int fd, dirlen = directory_size(filename);
 
@@ -1602,7 +1613,7 @@ static int write_loose_object(const struct object_id *oid, char *hdr,
 	strbuf_reset(&filename);
 	sha1_file_name(&filename, oid->hash);
 
-	fd = create_tmpfile(&tmp_file, filename.buf);
+	fd = create_object_tmpfile(&tmp_file, filename.buf);
 	if (fd < 0) {
 		if (errno == EACCES)
 			return error("insufficient permission for adding an object to repository database %s", get_object_directory());
