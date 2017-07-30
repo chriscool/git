521930522bc1ae32fc69e5ddb9491723c491ba38
Christian Couder <chriscool@tuxfamily.org>
2017-07-30 14:01:35 +0200

    remote-odb: use 'odb=magic' attribute to mark odb blobs
    To tell which blobs should be sent to the "magic" remote odb,
    let's require that the blobs be marked using the 'odb=magic'
    attribute.
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- t/t0570-read-object-http-e-odb.sh
+++ t/t0570-read-object-http-e-odb.sh
@@ -62,6 +62,9 @@ test_expect_success 'can delete uploaded files' '
 FILES_DIR="httpd/www/files"
 
 test_expect_success 'new blobs are transfered to the http server' '
+	echo "* odb=magic" >.gitattributes &&
+	GIT_NO_REMOTE_ODB=1 git add .gitattributes &&
+	GIT_NO_REMOTE_ODB=1 git commit -m "Add .gitattributes" &&
 	test_commit one &&
 	hash1=$(git ls-tree HEAD | grep one.t | cut -f1 | cut -d\  -f3) &&
 	echo "$hash1-4-blob" >expected &&
