a4bd704ec179aa2885a3b51cee0512440be2d866
Christian Couder <chriscool@tuxfamily.org>
2017-07-30 14:01:35 +0200

    external-odb: use 'odb=magic' attribute to mark odb blobs
    To tell which blobs should be sent to the "magic" external odb,
    let's require that the blobs be marked using the 'odb=magic'
    attribute.
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- external-odb.h
+++ external-odb.h
@@ -6,6 +6,7 @@ int external_odb_has_object(const unsigned char *sha1);
 int external_odb_get_object(const unsigned char *sha1);
 int external_odb_get_direct(const unsigned char *sha1);
 int external_odb_put_object(const void *buf, size_t len,
-			    const char *type, unsigned char *sha1);
+			    const char *type, unsigned char *sha1,
+			    const char *path);
 
 #endif /* EXTERNAL_ODB_H */
