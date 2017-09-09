f4b6e85d089e27ab6e69a1ee1a2a1e9545297ecf
Christian Couder <chriscool@tuxfamily.org>
2017-09-09 21:36:28 +0200

    upload.sh: fix indent and param substitution
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- t/lib-httpd/upload.sh
+++ t/lib-httpd/upload.sh
@@ -11,35 +11,35 @@ IFS="$OLDIFS"
 
 while test $# -gt 0
 do
-    key=${1%=*}
-    val=${1#*=}
+	key=${1%%=*}
+	val=${1#*=}
 
-    case "$key" in
+	case "$key" in
 	"sha1") sha1="$val" ;;
 	"type") type="$val" ;;
 	"size") size="$val" ;;
 	"delete") delete=1 ;;
 	*) echo >&2 "unknown key '$key'" ;;
-    esac
+	esac
 
-    shift
+	shift
 done
 
 case "$REQUEST_METHOD" in
-  POST)
-    if test "$delete" = "1"
-    then
-	rm -f "$FILES_DIR/$sha1-$size-$type"
-    else
-	mkdir -p "$FILES_DIR"
-	cat >"$FILES_DIR/$sha1-$size-$type"
-    fi
+POST)
+	if test "$delete" = "1"
+	then
+		rm -f "$FILES_DIR/$sha1-$size-$type"
+	else
+		mkdir -p "$FILES_DIR"
+		cat >"$FILES_DIR/$sha1-$size-$type"
+	fi
 
-    echo 'Status: 204 No Content'
-    echo
-    ;;
+	echo 'Status: 204 No Content'
+	echo
+	;;
 
-  *)
-    echo 'Status: 405 Method Not Allowed'
-    echo
+*)
+	echo 'Status: 405 Method Not Allowed'
+	echo
 esac
