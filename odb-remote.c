1b2dbf2dc80af2bbdda8153f46af9e385bff6ed0
Christian Couder <chriscool@tuxfamily.org>
2017-12-04 16:15:10 +0100

    Add initial odb remote support
    The odb-remote.{c,h} files will contain the functions
    that are called by the rest of Git mostly from
    "sha1_file.c" to access the objects managed by the
    odb remotes.
    
    The odb-helper.{c,h} files will contain the functions to
    actually implement communication with either the internal
    functions or the external scripts or processes that will
    manage and provide remote git objects.
    
    For now only infrastructure to create helpers from the
    config is implemented.
    
    Helped-by: Jeff King <peff@peff.net>
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

---
+++ odb-remote.c
@@ -0,0 +1,59 @@
+#include "cache.h"
+#include "odb-remote.h"
+#include "odb-helper.h"
+#include "config.h"
+
+static struct odb_helper *helpers;
+static struct odb_helper **helpers_tail = &helpers;
+
+static struct odb_helper *find_or_create_helper(const char *name, int len)
+{
+	struct odb_helper *o;
+
+	for (o = helpers; o; o = o->next)
+		if (!strncmp(o->name, name, len) && !o->name[len])
+			return o;
+
+	o = odb_helper_new(name, len);
+	*helpers_tail = o;
+	helpers_tail = &o->next;
+
+	return o;
+}
+
+static int odb_remote_config(const char *var, const char *value, void *data)
+{
+	struct odb_helper *o;
+	const char *name;
+	int namelen;
+	const char *subkey;
+
+	if (parse_config_key(var, "odb", &name, &namelen, &subkey) < 0)
+		return 0;
+
+	o = find_or_create_helper(name, namelen);
+
+	if (!strcmp(subkey, "promisorremote"))
+		return git_config_string(&o->dealer, var, value);
+
+	return 0;
+}
+
+static void odb_remote_init(void)
+{
+	static int initialized;
+
+	if (initialized)
+		return;
+	initialized = 1;
+
+	git_config(odb_remote_config, NULL);
+}
+
+const char *odb_remote_root(void)
+{
+	static const char *root;
+	if (!root)
+		root = git_pathdup("objects/external");
+	return root;
+}
---
+++ odb-remote.h
@@ -0,0 +1,6 @@
+#ifndef ODB_REMOTE_H
+#define ODB_REMOTE_H
+
+extern const char *odb_remote_root(void);
+
+#endif /* ODB_REMOTE_H */
