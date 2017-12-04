03ece764eb6f3a47578489535f2537c44ae24e60
Christian Couder <chriscool@tuxfamily.org>
2017-12-04 16:23:57 +0100

    odb-remote: add script mode support
    This adds support for the script command mode where
    an helper script or command is called to retrieve or
    manage objects.
    
    This implements the 'have' and 'get_git_obj'
    instructions for the script mode.
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- odb-helper.h
+++ odb-helper.h
@@ -9,6 +9,7 @@
 struct odb_helper {
 	const char *name;       /* from odb.<NAME>.<property> config entries */
 	const char *remote;     /* remote storing promised objects */
+	const char *command;    /* script command */
 	const char *partial_clone_filter; /* odb.<name>.partialCloneFilter */
 	enum odb_helper_type type;
 
@@ -27,6 +28,9 @@ struct odb_helper {
 extern struct odb_helper *odb_helper_new(const char *name, int namelen);
 extern int odb_helper_has_object(struct odb_helper *o,
 				 const unsigned char *sha1);
+extern int odb_helper_get_object(struct odb_helper *o,
+				 const unsigned char *sha1,
+				 int fd);
 extern int odb_helper_get_direct(struct odb_helper *o,
 				 const unsigned char *sha1);
 extern int odb_helper_get_many_direct(struct odb_helper *o,
