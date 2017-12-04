d178811a8e68119ce05f187dc633ce742be07383
Christian Couder <chriscool@tuxfamily.org>
2017-12-04 16:23:57 +0100

    real initial external odb support
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- external-odb.h
+++ external-odb.h
@@ -4,6 +4,7 @@
 extern int has_external_odb(void);
 extern const char *external_odb_root(void);
 extern int external_odb_has_object(const unsigned char *sha1);
+extern int external_odb_get_object(const unsigned char *sha1)
 extern int external_odb_get_direct(const unsigned char *sha1);
 
 #endif /* EXTERNAL_ODB_H */
