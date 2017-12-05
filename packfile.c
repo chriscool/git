cc0e3910b3aa37c20a447c10cedad5b72be79374
Jonathan Tan <jonathantanmy@google.com>
2017-12-05 16:58:44 +0000

    fsck: introduce promisor objects
    Currently, Git does not support repos with very large numbers of objects
    or repos that wish to minimize manipulation of certain blobs (for
    example, because they are very large) very well, even if the user
    operates mostly on part of the repo, because Git is designed on the
    assumption that every referenced object is available somewhere in the
    repo storage. In such an arrangement, the full set of objects is usually
    available in remote storage, ready to be lazily downloaded.
    
    Teach fsck about the new state of affairs. In this commit, teach fsck
    that missing promisor objects referenced from the reflog are not an
    error case; in future commits, fsck will be taught about other cases.
    
    Signed-off-by: Jonathan Tan <jonathantanmy@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- packfile.c
+++ packfile.c
@@ -13,6 +13,7 @@
 #include "tag.h"
 #include "tree-walk.h"
 #include "tree.h"
+#include "external-odb.h"
 
 char *odb_pack_name(struct strbuf *buf,
 		    const unsigned char *sha1,
@@ -1942,7 +1943,7 @@ int is_promisor_object(const struct object_id *oid)
 	static int promisor_objects_prepared;
 
 	if (!promisor_objects_prepared) {
-		if (repository_format_partial_clone) {
+		if (has_external_odb()) {
 			for_each_packed_object(add_promisor_object,
 					       &promisor_objects,
 					       FOR_EACH_OBJECT_PROMISOR_ONLY);
