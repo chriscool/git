5d7c169daa28b7032e915ea26011f798cf63658c
Christian Couder <chriscool@tuxfamily.org>
2017-12-07 10:50:36 +0100

    external-odb: add has_external_odb()
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- external-odb.h
+++ external-odb.h
@@ -1,6 +1,7 @@
 #ifndef EXTERNAL_ODB_H
 #define EXTERNAL_ODB_H
 
+extern int has_external_odb(void);
 extern const char *external_odb_root(void);
 extern int external_odb_has_object(const unsigned char *sha1);
 
