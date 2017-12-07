eb9edfce15eb59e9d646efdbcc259fbec0cb9245
Christian Couder <chriscool@tuxfamily.org>
2017-12-07 19:20:34 +0100

    external-odb: implement external_odb_get_direct
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- odb-helper.h
+++ odb-helper.h
@@ -20,5 +20,7 @@ struct odb_helper {
 extern struct odb_helper *odb_helper_new(const char *name, int namelen);
 extern int odb_helper_has_object(struct odb_helper *o,
 				 const unsigned char *sha1);
-
+extern int odb_helper_get_direct(struct odb_helper *o,
+				 const unsigned char *sha1,
+				 int fd);
 #endif /* ODB_HELPER_H */
