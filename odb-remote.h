2e3f7f8dfc89dd9508059725e105ac268b03c2dc
Christian Couder <chriscool@tuxfamily.org>
2017-12-07 19:20:34 +0100

    odb-remote: implement odb_remote_get_direct()
    This is implemented only in the promisor remote mode
    for now by calling fetch_object().
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- odb-remote.h
+++ odb-remote.h
@@ -3,5 +3,6 @@
 
 extern struct odb_helper *find_odb_helper(const char *dealer);
 extern int has_odb_remote(void);
+extern int odb_remote_get_direct(const unsigned char *sha1);
 
 #endif /* ODB_REMOTE_H */
