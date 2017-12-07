f99b984b7d68d4330d54addff497f2bfdb31da99
Christian Couder <chriscool@tuxfamily.org>
2017-12-07 19:20:34 +0100

    external-odb: implement external_odb_get_direct
    This is implemented only in the promisor remote mode
    for now by calling fetch_object().
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- odb-remote.c
+++ odb-remote.c
@@ -76,3 +76,18 @@ int odb_remote_has_object(const unsigned char *sha1)
 			return 1;
 	return 0;
 }
+
+int odb_remote_get_direct(const unsigned char *sha1)
+{
+	struct odb_helper *o;
+
+	odb_remote_init();
+
+	for (o = helpers; o; o = o->next) {
+		if (odb_helper_get_direct(o, sha1) < 0)
+			continue;
+		return 0;
+	}
+
+	return -1;
+}
--- odb-remote.h
+++ odb-remote.h
@@ -4,5 +4,6 @@
 extern int has_odb_remote(void);
 extern const char *odb_remote_root(void);
 extern int odb_remote_has_object(const unsigned char *sha1);
+extern int odb_remote_get_direct(const unsigned char *sha1);
 
 #endif /* ODB_REMOTE_H */
