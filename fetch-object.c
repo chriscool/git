927861a55a32cff53b1e2a550d3996941c0f9eb2
Jonathan Tan <jonathantanmy@google.com>
2017-12-08 15:58:47 +0000

    unpack-trees: batch fetching of missing blobs
    When running checkout, first prefetch all blobs that are to be updated
    but are missing. This means that only one pack is downloaded during such
    operations, instead of one per missing blob.
    
    This operates only on the blob level - if a repository has a missing
    tree, they are still fetched one at a time.
    
    This does not use the delayed checkout mechanism introduced in commit
    2841e8f ("convert: add "status=delayed" to filter process protocol",
    2017-06-30) due to significant conceptual differences - in particular,
    for partial clones, we already know what needs to be fetched based on
    the contents of the local repo alone, whereas for status=delayed, it is
    the filter process that tells us what needs to be checked in the end.
    
    Signed-off-by: Jonathan Tan <jonathantanmy@google.com>
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- fetch-object.c
+++ fetch-object.c
@@ -6,14 +6,13 @@
 #include "fetch-object.h"
 
 /*
- * TODO Investigate having fetch_object() return error/success,
+ * TODO Investigate having fetch_refs() return error/success,
  * changing callers.
  */
-void fetch_object(const char *remote_name, const unsigned char *sha1)
+static void fetch_refs(const char *remote_name, struct ref *ref)
 {
 	struct remote *remote;
 	struct transport *transport;
-	struct ref *ref;
 	int original_fetch_if_missing = fetch_if_missing;
 
 	fetch_if_missing = 0;
@@ -22,10 +21,29 @@ void fetch_object(const char *remote_name, const unsigned char *sha1)
 		die(_("Remote with no URL"));
 	transport = transport_get(remote, remote->url[0]);
 
-	ref = alloc_ref(sha1_to_hex(sha1));
-	hashcpy(ref->old_oid.hash, sha1);
 	transport_set_option(transport, TRANS_OPT_FROM_PROMISOR, "1");
 	transport_set_option(transport, TRANS_OPT_NO_DEPENDENTS, "1");
 	transport_fetch_refs(transport, ref);
 	fetch_if_missing = original_fetch_if_missing;
+}
+
+void fetch_object(const char *remote_name, const unsigned char *sha1)
+{
+	struct ref *ref = alloc_ref(sha1_to_hex(sha1));
+	hashcpy(ref->old_oid.hash, sha1);
+	fetch_refs(remote_name, ref);
+}
+
+void fetch_objects(const char *remote_name, const struct oid_array *to_fetch)
+{
+	struct ref *ref = NULL;
+	int i;
+
+	for (i = 0; i < to_fetch->nr; i++) {
+		struct ref *new_ref = alloc_ref(oid_to_hex(&to_fetch->oid[i]));
+		oidcpy(&new_ref->old_oid, &to_fetch->oid[i]);
+		new_ref->next = ref;
+		ref = new_ref;
+	}
+	fetch_refs(remote_name, ref);
 }
