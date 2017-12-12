adcc481609b3c258a1d05831dc15f4190fe34f29
Christian Couder <chriscool@tuxfamily.org>
2017-12-12 08:44:58 +0100

    odb-helper: add 'enum odb_helper_type'
    As there will be different kinds of helpers, let's add
    an "enum odb_helper_type" to tell between the different
    kinds.
    
    Let's add a field with this type in "struct odb_helper",
    and set it when reading the config file.
    
    While at it let's also make it possible to find an helper
    of a specific kind by adding a 'type' parameter in the
    find_odb_helper() function.
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- remote-odb.h
+++ remote-odb.h
@@ -1,8 +1,17 @@
 #ifndef REMOTE_ODB_H
 #define REMOTE_ODB_H
 
+enum odb_helper_type {
+	ODB_HELPER_ANY = 0,
+	ODB_HELPER_GIT_REMOTE,
+	ODB_HELPER_SCRIPT_CMD,
+	ODB_HELPER_SUBPROCESS_CMD,
+	OBJ_HELPER_MAX
+};
+
 extern void remote_odb_reinit(void);
-extern struct odb_helper *find_odb_helper(const char *remote);
+extern struct odb_helper *find_odb_helper(const char *remote,
+					  enum odb_helper_type type);
 extern int has_remote_odb(void);
 extern const char *remote_odb_root(void);
 extern int remote_odb_has_object(const unsigned char *sha1);
