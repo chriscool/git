2d2ded89d43ab1d8d2d164eb6c1e20d2f2506f51
Christian Couder <chriscool@tuxfamily.org>
2018-02-13 17:22:32 +0100

    external-odb: improve find_odb_helper()

--- external-odb.h
+++ external-odb.h
@@ -1,8 +1,11 @@
 #ifndef EXTERNAL_ODB_H
 #define EXTERNAL_ODB_H
 
+#include "odb-helper.h"
+
 extern int has_external_odb(void);
-extern struct odb_helper *find_odb_helper(const char *dealer);
+extern struct odb_helper *find_odb_helper(const char *dealer,
+					  enum odb_helper_type type);
 extern const char *external_odb_root(void);
 extern int external_odb_has_object(const unsigned char *sha1);
 extern int external_odb_get_object(const unsigned char *sha1);
