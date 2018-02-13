05b8a0171bbeb3d07868e344f8badd54ba79b7b1
Christian Couder <chriscool@tuxfamily.org>
2018-02-13 17:56:07 +0100

    external-odb: add external_odb_get_many_direct()
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- external-odb.h
+++ external-odb.h
@@ -17,5 +17,6 @@ extern const char *external_odb_root(void);
 extern int external_odb_has_object(const unsigned char *sha1);
 extern int external_odb_get_object(const unsigned char *sha1);
 extern int external_odb_get_direct(const unsigned char *sha1);
+extern int external_odb_get_many_direct(const struct oid_array *to_get);
 
 #endif /* EXTERNAL_ODB_H */
