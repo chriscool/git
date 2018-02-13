26b77c0aae003b32cad0514055b9d4e5e5e6114d
Christian Couder <chriscool@tuxfamily.org>
2018-02-13 17:56:07 +0100

    odb-remote: add odb_remote_get_many_direct()
    This function will be used to get many objects from a promisor
    remote.
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- odb-remote.h
+++ odb-remote.h
@@ -3,5 +3,6 @@
 
 extern const char *odb_remote_root(void);
 extern int odb_remote_get_direct(const unsigned char *sha1);
+extern int odb_remote_get_many_direct(const struct oid_array *to_get);
 
 #endif /* ODB_REMOTE_H */
