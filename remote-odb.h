23f25d791ddf342c94c5220e22b5d66ccc124fd8
Christian Couder <chriscool@tuxfamily.org>
2018-03-11 08:36:16 +0100

    sha1_file: add prepare_external_alt_odb()
    This new function adds the external odb cache to all
    the other odbs.
    
    Let's add the remote_odb_root() helper at the same time.
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- remote-odb.h
+++ remote-odb.h
@@ -4,6 +4,7 @@
 extern void remote_odb_reinit(void);
 extern struct odb_helper *find_odb_helper(const char *remote);
 extern int has_remote_odb(void);
+extern const char *remote_odb_root(void);
 extern int remote_odb_has_object(const unsigned char *sha1);
 extern int remote_odb_get_direct(const struct object_id *oids, int oid_nr);
 
