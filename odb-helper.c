6bd80d011ddb89a6cbe0207c3a4db4265caec172
Christian Couder <chriscool@tuxfamily.org>
2018-04-12 13:35:08 +0200

    odb-helper: add initial "have" support
    Add initial support for "have" related functions.
    
    Helped-by: Jeff King <peff@peff.net>
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- odb-helper.c
+++ odb-helper.c
@@ -16,6 +16,43 @@ struct odb_helper *odb_helper_new(const char *name, int namelen)
 	return o;
 }
 
+struct odb_helper_cmd {
+	struct argv_array argv;
+	struct child_process child;
+};
+
+static void odb_helper_load_have(struct odb_helper *o)
+{
+	if (o->have_valid)
+		return;
+	o->have_valid = 1;
+
+	/* TODO */
+}
+
+static const unsigned char *have_sha1_access(size_t index, void *table)
+{
+	struct odb_helper_object *have = table;
+	return have[index].oid.hash;
+}
+
+static struct odb_helper_object *odb_helper_lookup(struct odb_helper *o,
+						   const unsigned char *sha1)
+{
+	int idx;
+
+	odb_helper_load_have(o);
+	idx = sha1_pos(sha1, o->have, o->have_nr, have_sha1_access);
+	if (idx < 0)
+		return NULL;
+	return &o->have[idx];
+}
+
+int odb_helper_has_object(struct odb_helper *o, const unsigned char *sha1)
+{
+	return !!odb_helper_lookup(o, sha1);
+}
+
 int odb_helper_get_direct(struct odb_helper *o,
 			  const struct object_id *oids,
 			  int oid_nr)
--- odb-helper.h
+++ odb-helper.h
@@ -12,10 +12,21 @@ struct odb_helper {
 	const char *remote;               /* odb.<NAME>.promisorRemote */
 	const char *partial_clone_filter; /* odb.<NAME>.partialCloneFilter */
 
+	struct odb_helper_object {
+		struct object_id oid;
+		unsigned long size;
+		enum object_type type;
+	} *have;
+	int have_nr;
+	int have_alloc;
+	int have_valid;
+
 	struct odb_helper *next;
 };
 
 extern struct odb_helper *odb_helper_new(const char *name, int namelen);
+extern int odb_helper_has_object(struct odb_helper *o,
+				 const unsigned char *sha1);
 extern int odb_helper_get_direct(struct odb_helper *o,
 				 const struct object_id *oids,
 				 int oid_nr);
