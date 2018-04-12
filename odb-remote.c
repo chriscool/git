958eb24fb1d235c5df525dd2c04e27f2ce2e8553
Christian Couder <chriscool@tuxfamily.org>
2018-04-12 13:35:08 +0200

    odb-helper: add initial "have" support
    Add initial support for "have" related functions.
    
    Helped-by: Jeff King <peff@peff.net>
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- odb-remote.c
+++ odb-remote.c
@@ -83,6 +83,18 @@ int has_odb_remote(void)
 	return !!find_odb_helper(NULL);
 }
 
+int odb_remote_has_object(const unsigned char *sha1)
+{
+	struct odb_helper *o;
+
+	odb_remote_init();
+
+	for (o = helpers; o; o = o->next)
+		if (odb_helper_has_object(o, sha1))
+			return 1;
+	return 0;
+}
+
 int odb_remote_get_direct(const unsigned char *sha1)
 {
 	struct odb_helper *o;
