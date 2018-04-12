5dd5150236669a5212802b351af9e8cc2fc79409
Christian Couder <chriscool@tuxfamily.org>
2018-04-12 13:35:08 +0200

    odb-helper: add initial "have" support
    Add initial support for "have" related functions.
    
    Helped-by: Jeff King <peff@peff.net>
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- odb-helper.h
+++ odb-helper.h
@@ -5,10 +5,21 @@ struct odb_helper {
 	const char *name;
 	const char *dealer;
 
+	struct odb_helper_object {
+		struct object_id oid;
+		unsigned long size;
+		enum object_type type;
+	} *have;
+	int have_nr;
+	int have_alloc;
+	int have_valid;
+
 	struct odb_helper *next;
 };
 
 extern struct odb_helper *odb_helper_new(const char *name, int namelen);
+extern int odb_helper_has_object(struct odb_helper *o,
+				 const unsigned char *sha1);
 extern int odb_helper_get_direct(struct odb_helper *o,
 				 const unsigned char *sha1);
 extern int odb_helper_get_many_direct(struct odb_helper *o,
--- odb-remote.h
+++ odb-remote.h
@@ -2,6 +2,7 @@
 #define ODB_REMOTE_H
 
 extern const char *odb_remote_root(void);
+extern int odb_remote_has_object(const unsigned char *sha1);
 extern int odb_remote_get_direct(const unsigned char *sha1);
 extern int odb_remote_get_many_direct(const struct oid_array *to_get);
 
