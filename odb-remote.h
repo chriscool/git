2fa420cee20e5f6fc96c52c1d881e513229189d6
Christian Couder <chriscool@tuxfamily.org>
2018-04-29 22:34:58 +0200

    odb-remote: add odb_remote_reinit()

--- odb-remote.h
+++ odb-remote.h
@@ -1,6 +1,7 @@
 #ifndef ODB_REMOTE_H
 #define ODB_REMOTE_H
 
+extern void odb_remote_reinit(void);
 extern struct odb_helper *find_odb_helper(const char *dealer);
 extern int has_odb_remote(void);
 extern const char *odb_remote_root(void);
