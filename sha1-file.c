5e55ef80f8968b56f25699b79f5988daf3b884af
Christian Couder <christian.couder@gmail.com>
2018-06-30 10:35:40 +0200

    Use remote.*.partialclonefilter
    Let's make the partial clone filter specific to one promisor
    remote instead of general to all the promisor remotes.
    
    This makes it possible to have different partial clone
    filters for different promisor remotes.
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- sha1-file.c
+++ sha1-file.c
@@ -1313,10 +1313,10 @@ int oid_object_info_extended(struct repository *r, const struct object_id *oid,
 		if (fetch_if_missing && has_promisor_remote() &&
 		    !already_retried && r == the_repository) {
 			/*
-			 * TODO Investigate checking promisor_remote_get_direct()
+			 * TODO Investigate checking promisors_get_direct()
 			 * TODO return value and stopping on error here.
 			 * TODO Pass a repository struct through
-			 * promisor_remote_get_direct(), such that arbitrary
+			 * promisors_get_direct(), such that arbitrary
 			 * repositories work.
 			 */
 			promisors_get_direct(real, 1);
