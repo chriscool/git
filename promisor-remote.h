a4b71fee62d3d546f1c6a19dc935dc1b1c64b994
Christian Couder <christian.couder@gmail.com>
2018-06-30 10:35:36 +0200

    promisor-remote: implement promisor_remote_get_direct()
    This is implemented for now by calling fetch_objects(). It fetches
    from all the promisor remotes.
    
    Helped-by: Ramsay Jones <ramsay@ramsayjones.plus.com>
    Helped-by: Derrick Stolee <stolee@gmail.com>
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- promisor-remote.h
+++ promisor-remote.h
@@ -1,6 +1,9 @@
 #ifndef PROMISOR_REMOTE_H
 #define PROMISOR_REMOTE_H
 
+struct object_id;
+struct repository;
+
 /*
  * Promisor remote linked list
  * Its information come from remote.XXX config entries.
@@ -12,5 +15,8 @@ struct promisor_remote {
 
 extern struct promisor_remote *promisor_remote_find(const char *remote_name);
 extern int has_promisor_remote(void);
+extern int promisor_remote_get_direct(struct repository *repo,
+				      const struct object_id *oids,
+				      int oid_nr);
 
 #endif /* PROMISOR_REMOTE_H */
