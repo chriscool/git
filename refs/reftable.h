59035ee14589a29defd11d7ebdfda0f49f3b0636
Christian Couder <chriscool@tuxfamily.org>
2018-07-02 10:10:51 +0200

    refs: initial reftable implementation

---
+++ refs/reftable.h
@@ -0,0 +1,15 @@
+#ifndef REFTABLE_H
+#define REFTABLE_H
+
+struct reftable_header;
+struct ref_update;
+
+void reftable_header_init(struct reftable_header *header, uint32_t block_size,
+			  uint64_t min_update_index, uint64_t max_update_index);
+int reftable_write_reftable_blocks(int fd, uint32_t block_size,
+				   const struct ref_update **updates, int nr_updates);
+
+#endif /* REFTABLE_H */
+
+
+
