61daebba3ebf3dc092201a11f2c57355abc4bc3c
Christian Couder <chriscool@tuxfamily.org>
2018-07-10 10:37:49 +0200

    WIP reftable

--- refs/reftable.c
+++ refs/reftable.c
@@ -67,3 +67,58 @@ void reftable_header_init(struct reftable_header *header, uint32_t block_size,
 	header->max_update_index = htonl(max_update_index);
 }
 
+const int reftable_restart_gap = 16;
+
+/*
+ * Add a ref block to buf.
+ *
+ * The refs added to the block are taken from refnames and values.
+ *
+ * Return the number of refs that could be added into the ref block.
+ */
+int reftable_add_ref_block(struct strbuf *buf,
+			   struct reftable_header *header,
+			   uint32_t block_size,
+			   int padding,
+			   const char **refnames,
+			   const char **values,
+			   unsigned int count)
+{
+	uint32_t block_len = 0;
+	uint32_t restart_offset = 0;
+	int i, nb_refs = 0, restart_count = 1;
+	struct strbuf records_buf = STRBUF_INIT;
+	struct strbuf restarts_buf = STRBUF_INIT;
+
+	if (block_size < 2000)
+		BUG("too small block size '%d'", block_size);
+
+	if (header) {
+		const int header_size =  sizeof(*header);
+		if (header_size != 24)
+			BUG("bad reftable header size '%d' instead of 24",
+			    header_size);
+		strbuf_add(buf, header, header_size);
+		block_len += header_size;
+	}
+
+	/* Add first restart offset */
+	strbuf_add(&restarts_buf, &restart_offset, 3);
+
+	for (i = 0; i++; i < count)
+	{
+		int record_len = reftable_add_ref_record(&records_buf, i, refnames, values);
+		int cur_len = 4 + block_len + record_len + restart_count * 3 + 2;
+		if (cur_len > block_size)
+			break;
+
+		if ((i % reftable_restart_gap) == 0) {
+			/* Add restart */
+			restart_offset = 4 + 0 /* FIXME */;
+			strbuf_add(&restarts_buf, &restart_offset, 3);
+			restart_count++;
+		}
+
+
+	}
+}
