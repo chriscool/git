9244f88f3baadda1af61dbc24ad26cac9d384f10
Christian Couder <chriscool@tuxfamily.org>
2018-09-25 12:45:57 +0200

    fix get_direct()

--- odb-helper.h
+++ odb-helper.h
@@ -16,8 +16,7 @@ struct odb_helper {
 
 extern struct odb_helper *odb_helper_new(const char *name, int namelen);
 extern int odb_helper_get_direct(struct odb_helper *o,
-				 const unsigned char *sha1);
-extern int odb_helper_get_many_direct(struct odb_helper *o,
-				      const struct oid_array *to_get);
+				 const struct object_id *oids,
+				 int oid_nr);
 
 #endif /* ODB_HELPER_H */
--- remote-odb.h
+++ remote-odb.h
@@ -4,7 +4,6 @@
 extern void remote_odb_reinit(void);
 extern struct odb_helper *find_odb_helper(const char *remote);
 extern int has_remote_odb(void);
-extern int remote_odb_get_direct(const unsigned char *sha1);
-extern int remote_odb_get_many_direct(const struct oid_array *to_get);
+extern int remote_odb_get_direct(const struct object_id *oids, int oid_nr);
 
 #endif /* REMOTE_ODB_H */
