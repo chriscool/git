3e91cdc2b818425b5049b8c6de7477e84b309e16
Christian Couder <chriscool@tuxfamily.org>
2019-01-17 16:22:13 +0100

    reftable: add infrastructure for reading ref blocks

--- refs/reftable.h
+++ refs/reftable.h
@@ -8,6 +8,9 @@ void reftable_header_init(struct reftable_header *header, uint32_t block_size,
 			  uint64_t min_update_index, uint64_t max_update_index);
 int reftable_write_reftable_blocks(int fd, uint32_t block_size, const char *path,
 				   const struct ref_update **updates, int nr_updates);
+int reftable_read_reftable_blocks(int fd, uint32_t block_size, const char *path,
+				  const struct ref_update **updates,
+				  int *nr_updates, int *alloc_updates);
 
 #endif /* REFTABLE_H */
 
