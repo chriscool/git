8a0a63d384bd0178f3101f2ecf0c49678942b9c0
Christian Couder <chriscool@tuxfamily.org>
2019-01-17 16:21:09 +0100

    reftable: improve writing ref blocks

--- refs/reftable.h
+++ refs/reftable.h
@@ -6,7 +6,7 @@ struct ref_update;
 
 void reftable_header_init(struct reftable_header *header, uint32_t block_size,
 			  uint64_t min_update_index, uint64_t max_update_index);
-int reftable_write_reftable_blocks(int fd, uint32_t block_size,
+int reftable_write_reftable_blocks(int fd, uint32_t block_size, const char *path,
 				   const struct ref_update **updates, int nr_updates);
 
 #endif /* REFTABLE_H */
