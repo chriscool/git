19c24e56f1a15337df830a0b06e84459c730e837
Christian Couder <chriscool@tuxfamily.org>
2019-02-14 08:20:31 +0100

    reftable: implement reftable_header_check()

--- refs/reftable.h
+++ refs/reftable.h
@@ -4,8 +4,6 @@
 struct reftable_header;
 struct ref_update;
 
-void reftable_header_init(struct reftable_header *header, uint32_t block_size,
-			  uint64_t min_update_index, uint64_t max_update_index);
 int reftable_write_reftable_blocks(int fd, uint32_t block_size, const char *path,
 				   const struct ref_update **updates, int nr_updates);
 int reftable_read_reftable_blocks(int fd, uint32_t block_size, const char *path,
