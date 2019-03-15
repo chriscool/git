cac069e8fe97ab36526ffc8ca05a25033cb22a75
Christian Couder <chriscool@tuxfamily.org>
2019-03-15 07:58:29 +0100

    reftable: add long reftable format test

--- t/t3220-reftable-basics.sh
+++ t/t3220-reftable-basics.sh
@@ -19,4 +19,20 @@ test_expect_success 'simple setup with 1 branch and 2 tags' '
 	test_cmp expected actual
 '
 
+test_expect_success 'add many branches' '
+	for i in $(test_seq 1 5000)
+	do
+		git checkout -b "br$i" || break
+		echo "refs/heads/br$i" >>raw-expected || break
+	done &&
+	echo "refs/heads/master" >>raw-expected &&
+	echo "refs/tags/first" >>raw-expected &&
+	echo "refs/tags/second" >>raw-expected &&
+	sort raw-expected >expected &&
+	test-tool reftable write-reftable reftable &&
+	test-tool reftable read-reftable reftable >raw-actual &&
+	sort raw-actual >actual &&
+	test_cmp expected actual
+'
+
 test_done
