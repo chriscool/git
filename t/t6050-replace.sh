0f5a527142a38ff2ba08bc99a55a2c7a4fdcff35
Christian Couder <chriscool@tuxfamily.org>
2019-03-28 17:41:18 +0100

    t6050: add missing test for --graft with tag
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- t/t6050-replace.sh
+++ t/t6050-replace.sh
@@ -416,6 +416,19 @@ test_expect_success '--graft using a tag as the new parent' '
 	git replace -d $HASH7
 '
 
+test_expect_success '--graft using a tag as the replaced object' '
+	git tag replaced_object $HASH7 &&
+	git replace --graft replaced_object $HASH5 &&
+	commit_has_parents $HASH7 $HASH5 &&
+	git replace -d $HASH7 &&
+	git tag -a -m "annotated replaced object tag" annotated_replaced_object $HASH7 &&
+	test_must_fail git replace --graft annotated_replaced_object $HASH5 &&
+	test -z "$(git replace)" &&
+	git replace --graft annotated_replaced_object^{commit} $HASH5 &&
+	commit_has_parents $HASH7 $HASH5 &&
+	git replace -d $HASH7
+'
+
 test_expect_success GPG 'set up a signed commit' '
 	echo "line 17" >>hello &&
 	echo "line 18" >>hello &&
