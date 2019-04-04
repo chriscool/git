5d074419b95d76553d8d5044049597ae9c44737b
Christian Couder <chriscool@tuxfamily.org>
2019-04-05 01:53:20 +0200

    t3220: test annotated tag & peeled refs

--- t/t3220-reftable-basics.sh
+++ t/t3220-reftable-basics.sh
@@ -7,28 +7,53 @@ test_description='check reftable format'
 
 . ./test-lib.sh
 
+first_commit=
+second_commit=
+master_commit=
+
 test_expect_success 'simple setup with 1 branch and 2 tags' '
 	test_commit first &&
+	first_commit=$(git rev-parse first) &&
 	test_commit second &&
+	second_commit=$(git rev-parse second) &&
+	master_commit=$(git rev-parse master)
+'
+
+test_expect_success 'check writing and reading reftable' '
 	test-tool reftable write-reftable reftable &&
 	test-tool reftable read-reftable reftable >raw-actual &&
 	sort raw-actual >actual &&
-	echo "refs/heads/master $(git rev-parse master)" >>expected &&
-	echo "refs/tags/first $(git rev-parse first)" >>expected &&
-	echo "refs/tags/second $(git rev-parse second)" >>expected &&
+	echo "refs/heads/master $master_commit" >>expected &&
+	echo "refs/tags/first $first_commit" >>expected &&
+	echo "refs/tags/second $second_commit" >>expected &&
+	test_cmp expected actual
+'
+
+second_a_tag=
+
+test_expect_success 'add 1 annotated tags' '
+	git tag -a -m "annotated second" annotated_second master &&
+	test-tool reftable write-reftable reftable &&
+	test-tool reftable read-reftable reftable >raw-actual &&
+	sort raw-actual >actual &&
+	second_a_tag=$(git rev-parse annotated_second) &&
+	echo "refs/heads/master $master_commit" >expected &&
+	echo "refs/tags/annotated_second $second_a_tag $second_commit" >>expected &&
+	echo "refs/tags/first $first_commit" >>expected &&
+	echo "refs/tags/second $second_commit" >>expected &&
 	test_cmp expected actual
 '
 
 test_expect_success 'add many branches' '
-	HASH=$(git rev-parse master) &&
 	for i in $(test_seq 1 2000)
 	do
 		git checkout -b "br$i" || break
-		echo "refs/heads/br$i $HASH" >>raw-expected || break
+		echo "refs/heads/br$i $master_commit" >>raw-expected || break
 	done &&
-	echo "refs/heads/master $HASH" >>raw-expected &&
-	echo "refs/tags/first $(git rev-parse first)" >>raw-expected &&
-	echo "refs/tags/second $(git rev-parse second)" >>raw-expected &&
+	echo "refs/heads/master $master_commit" >>raw-expected &&
+	echo "refs/tags/annotated_second $second_a_tag $second_commit" >>raw-expected &&
+	echo "refs/tags/first $first_commit" >>raw-expected &&
+	echo "refs/tags/second $second_commit" >>raw-expected &&
 	sort raw-expected >expected &&
 	test-tool reftable write-reftable reftable &&
 	test-tool reftable read-reftable reftable >raw-actual &&
