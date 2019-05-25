7f2089c1845bb38fe99a786119cf5ca1fcc1fc66
Christian Couder <chriscool@tuxfamily.org>
2019-05-26 00:36:57 +0200

    psuh: display arguments
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- builtin/psuh.c
+++ builtin/psuh.c
@@ -3,6 +3,17 @@
 
 int cmd_psuh(int argc, const char **argv, const char *prefix)
 {
-	printf(_("Pony saying hello goes here.\n"));
+	int i;
+
+	printf(Q_("Your args (there is %d):\n",
+		  "Your args (there are %d):\n",
+		  argc),
+	       argc);
+	for (i = 0; i < argc; i++)
+		printf("%d: %s\n", i, argv[i]);
+
+	printf(_("Your current working directory:\n<top-level>%s%s\n"),
+	       prefix ? "/" : "", prefix ? prefix : "");
+
 	return 0;
 }
