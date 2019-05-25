58c11e0637db312577139f680fd32df485bf6181
Christian Couder <chriscool@tuxfamily.org>
2019-05-26 00:47:50 +0200

    psuh: display user name from config
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- builtin/psuh.c
+++ builtin/psuh.c
@@ -1,9 +1,11 @@
 #include "cache.h"
 #include "builtin.h"
+#include "config.h"
 
 int cmd_psuh(int argc, const char **argv, const char *prefix)
 {
 	int i;
+	const char *cfg_name;
 
 	printf(Q_("Your args (there is %d):\n",
 		  "Your args (there are %d):\n",
@@ -14,6 +16,12 @@ int cmd_psuh(int argc, const char **argv, const char *prefix)
 
 	printf(_("Your current working directory:\n<top-level>%s%s\n"),
 	       prefix ? "/" : "", prefix ? prefix : "");
+
+	git_config(git_default_config, NULL);
+	if (git_config_get_string_const("user.name", &cfg_name) > 0)
+		printf(_("No name is found in config\n"));
+	else
+		printf(_("Your name: %s\n"), cfg_name);
 
 	return 0;
 }
