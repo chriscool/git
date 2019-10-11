3d5ecff55e8d1fe7d511bcc892f5122eb21e7113
Christian Couder <chriscool@tuxfamily.org>
2019-10-11 07:10:42 +0200

    pack-bitmap: consistently use 'reuse_out' for reuse bitmap

--- pack-bitmap.h
+++ pack-bitmap.h
@@ -51,7 +51,7 @@ struct bitmap_index *prepare_bitmap_walk(struct rev_info *revs);
 int reuse_partial_packfile_from_bitmap(struct bitmap_index *,
 				       struct packed_git **packfile,
 				       uint32_t *entries,
-				       struct bitmap **bitmap);
+				       struct bitmap **reuse_out);
 int rebuild_existing_bitmaps(struct bitmap_index *, struct packing_data *mapping,
 			     kh_oid_map_t *reused_bitmaps, int show_progress);
 void free_bitmap_index(struct bitmap_index *);
