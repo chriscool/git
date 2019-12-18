56bc0f910dbed3d81a2e8b8306c080cd98f6cc05
Christian Couder <chriscool@tuxfamily.org>
2019-12-18 06:43:01 +0100

    WIP3

--- builtin/pack-objects.c
+++ builtin/pack-objects.c
@@ -2681,7 +2681,9 @@ static void ll_find_deltas(struct object_entry **list, unsigned list_size,
 
 static int obj_is_packed(const struct object_id *oid)
 {
-	return !!packlist_find(&to_pack, oid);
+	return packlist_find(&to_pack, oid) ||
+		(reuse_packfile_bitmap &&
+		 bitmap_walk_contains(bitmap_git, reuse_packfile_bitmap, oid));
 }
 
 static void add_tag_chain(const struct object_id *oid)
