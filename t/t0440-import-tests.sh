3d90e8f4797c486e83488b11da64a4a6e31077e0
Christian Couder <chriscool@tuxfamily.org>
2020-07-05 06:06:15 +0200

    WIP import tests

---
+++ t/t0440-import-tests.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+# Copyright (c) 2020 Christian Couder
+#
+
+test_description='Test import and export commands'
+
+. ./test-lib.sh
+
+PATH="$TEST_DIRECTORY/t0440:$PATH"
+
+test_expect_success 'setup remote server repository' '
+	git init remote_server &&
+	(cd remote_server &&
+	 echo content >file &&
+	 git add file &&
+	 git commit -m one)
+'
+
+test_expect_success 'simple import' '
+	(cd remote_server &&
+	 git fast-export HEAD >../expect) &&
+	(cd remote_server &&
+	 my_git_export.sh HEAD >../actual) &&
+	test_cmp expect actual
+'
+
+exit 1
+
+test_done
---
+++ t/t0440/my_git_export.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+#
+# Copyright (c) 2020 Christian Couder
+#
+
+mark_tmp_file="/tmp/export_marks"
+
+my_init_marks() {
+	echo "1" >"$mark_tmp_file"
+}
+
+my_get_next_mark() {
+	mark=$(cat "$mark_tmp_file")
+	echo "$mark"
+	mark=$((mark+1))
+	echo "$mark" >"$mark_tmp_file"	
+}
+
+my_export_blob() {
+	blob="$1"
+	echo "blob"
+	echo "mark :$(my_get_next_mark)"
+	size=$(git cat-file -s "$blob") || return
+	echo "data $size"
+	git cat-file -p "$blob" || return
+}
+
+my_export_many_blob() {
+	while read blob
+	do
+		my_export_blob "$blob"
+	done
+}
+
+my_find_blobs() {
+	rev="$1"
+	git ls-tree "$rev" | while read mode type hash name
+	do
+		test "$type" = "blob" && echo "$hash"
+	done
+}
+
+my_reset_rev() {
+	rev="$1"
+	echo "reset $rev"
+}	
+
+my_export_commits() {
+	for rev in "$@"
+	do
+		contents_size=$(git for-each-ref --format="%(contents)" "$rev" | wc -c)
+		contents_size=$((contents_size-1))
+		git for-each-ref --format="commit %(refname)
+mark :$(my_get_next_mark)
+author %(author)
+committer %(committer)
+data $contents_size
+%(contents)" "$rev"
+	done
+}
+
+my_init_marks
+
+rev=$(git symbolic-ref "$1")
+
+my_find_blobs "$rev" | my_export_many_blob
+
+echo
+
+my_reset_rev "$rev"
+
+my_export_commits "$rev"
+
