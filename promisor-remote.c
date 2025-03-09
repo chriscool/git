59b67831e2e9f5dc71e66b572cb0181ad4b0ef11
Christian Couder <christian.couder@gmail.com>
2025-03-09 17:06:24 +0100

    promisor-remote: fix segfault when remote URL is missing
    Pushing NULL into a 'strvec' results in a segfault, as 'strvec' is a
    kind of NULL terminated array that is designed to be compatible with
    'argv' variables used on the command line.
    
    So when an URL is missing from the config, let's push an empty string
    instead of NULL into the 'strvec' that stores URLs.
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- promisor-remote.c
+++ promisor-remote.c
@@ -13,6 +13,7 @@
 #include "environment.h"
 #include "url.h"
 #include "version.h"
+#include "trace.h"
 
 struct promisor_remote_config {
 	struct promisor_remote *promisors;
@@ -323,11 +324,12 @@ static void promisor_info_vecs(struct repository *repo,
 	promisor_remote_init(repo);
 
 	for (r = repo->promisor_remote_config->promisors; r; r = r->next) {
-		char *url;
+		char *url = NULL;
 		char *url_key = xstrfmt("remote.%s.url", r->name);
+		const char *url_pushed = git_config_get_string(url_key, &url) ? "" : url;
 
 		strvec_push(names, r->name);
-		strvec_push(urls, git_config_get_string(url_key, &url) ? NULL : url);
+		strvec_push(urls, url_pushed);
 
 		free(url);
 		free(url_key);
@@ -356,7 +358,7 @@ char *promisor_remote_info(struct repository *repo)
 			strbuf_addch(&sb, ';');
 		strbuf_addstr(&sb, "name=");
 		strbuf_addstr_urlencode(&sb, names.v[i], allow_unsanitized);
-		if (urls.v[i]) {
+		if (urls.v[i] && *urls.v[i]) {
 			strbuf_addstr(&sb, ",url=");
 			strbuf_addstr_urlencode(&sb, urls.v[i], allow_unsanitized);
 		}
--- t/t5710-promisor-remote-capability.sh
+++ t/t5710-promisor-remote-capability.sh
@@ -193,6 +193,22 @@ test_expect_success "clone with 'KnownName' and different remote names" '
 	initialize_server 1 "$oid"
 '
 
+test_expect_success "clone with 'KnownName' and missing URL in the config" '
+	git -C server config promisor.advertise true &&
+
+	# Clone from server to create a client
+	GIT_NO_LAZY_FETCH=0 git clone -c remote.lop.promisor=true \
+		-c promisor.acceptfromserver=KnownName \
+		--no-local --filter="blob:limit=5k" server client &&
+	test_when_finished "rm -rf client" &&
+
+	# Check that the largest object is not missing on the server
+	check_missing_objects server 0 "" &&
+
+	# Reinitialize server so that the largest object is missing again
+	initialize_server 1 "$oid"
+'
+
 test_expect_success "clone with promisor.acceptfromserver set to 'KnownUrl'" '
 	git -C server config promisor.advertise true &&
 
